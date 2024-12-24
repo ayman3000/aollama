c42be544c705b22fe294335fc9a06f54de0b3867
Ayman Hamed <ayman3000@gmail.com>
2024-12-24 15:38:04 +0200

    added credits to ndrea Bizzotto

--- lib/widgets/platform_widgets/platform_widget.dart
+++ lib/widgets/platform_widgets/platform_widget.dart
@@ -1,9 +1,16 @@
+/// This `PlatformWidget` base class was created by an original contributor.
+/// It is designed to provide a base for platform-aware widgets, ensuring compatibility
+/// between Material and Cupertino designs.
+///
+/// Credits: Andrea Bizzotto
+/// Thank you for the inspiration and contribution!
+//// Ref: https://github.com/bizz84/platform_aware_widgets_flutter/blob/master/lib/common_widgets/platform_widget.dart
+
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
 // import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-// Ref: https://github.com/bizz84/platform_aware_widgets_flutter/blob/master/lib/common_widgets/platform_widget.dart
 /// Base class to be extended by all platform aware widgets
 abstract class PlatformWidget extends StatelessWidget {
   const PlatformWidget({super.key});
