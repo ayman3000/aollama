0bd33568f7bc8ece12e211c55c219796318e0227
Ayman Hamed <ayman3000@gmail.com>
2024-12-24 09:52:38 +0200

    added ref to PlatformWidget widget

--- lib/widgets/platform_widgets/platform_widget.dart
+++ lib/widgets/platform_widgets/platform_widget.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+// Ref: https://github.com/bizz84/platform_aware_widgets_flutter/blob/master/lib/common_widgets/platform_widget.dart
 /// Base class to be extended by all platform aware widgets
 abstract class PlatformWidget extends StatelessWidget {
   const PlatformWidget({super.key});
